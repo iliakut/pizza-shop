df0011c46273c11e3d054fc65324a322381fd53e
ilia <ilyakut@gmail.com>
2020-06-02 22:59:50 +0300

    added remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
