85a7e9481cad351f9708f63e7225148ded57a9f5
ilia <ilyakut@gmail.com>
2020-06-05 00:19:04 +0300

    Updates

---
+++ precache-manifest.3a5a3f7f25419d9e91071b3d038a582b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1eca28f9246e2090fbe9abc753b1cc",
+    "url": "/pizza-shop/index.html"
+  },
+  {
+    "revision": "94bfbdc4b7a0ee53a5ef",
+    "url": "/pizza-shop/static/css/main.5347c5e7.chunk.css"
+  },
+  {
+    "revision": "d214303759f9c4765ef9",
+    "url": "/pizza-shop/static/js/2.1c76f684.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pizza-shop/static/js/2.1c76f684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bfbdc4b7a0ee53a5ef",
+    "url": "/pizza-shop/static/js/main.84561467.chunk.js"
+  },
+  {
+    "revision": "5f5be6be6d31f5dc0eda",
+    "url": "/pizza-shop/static/js/runtime-main.8d4e47f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pizza-shop/precache-manifest.6c71c165e8b633ae3f0f8570178a8421.js"
+  "/pizza-shop/precache-manifest.3a5a3f7f25419d9e91071b3d038a582b.js"
 );
 
 self.addEventListener('message', (event) => {
