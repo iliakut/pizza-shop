f22afb05e47a334aa0cbc8a1c36e506f1e87fe00
ilia <ilyakut@gmail.com>
2020-06-04 23:54:11 +0300

    sidebar layout

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./sidebar.css"
 const Sidebar = () => {
   return (
-    <div className="sidenav">
-      <a href="#about">About</a>
+    <div className="sidebar rounded m-2 px-2 pb-2">
+      <button type="button" className="btn btn-primary mt-2">About</button>
+      <button type="button" className="btn btn-primary mt-2">About</button>
+      <button type="button" className="btn btn-primary mt-2">About</button>
     </div>
   );
 };
