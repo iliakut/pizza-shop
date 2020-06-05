c6033170418ebfcf564b98a841d6d4028f20be5f
ilia <ilyakut@gmail.com>
2020-06-05 20:14:48 +0300

    added cards

--- src/components/mainContent/mainContent.js
+++ src/components/mainContent/mainContent.js
@@ -2,16 +2,27 @@ import React from "react";
 import Card from "../../UI/card/card";
 
 const MainContent = ({menuItem}) => {
-  console.log(menuItem)
   const header = menuItem?.header;
   const menuPositions = menuItem?.items;
+  let Cards;
+
+  if (menuPositions) {
+    Cards = menuPositions.map(position => {
+      return (
+        <Card
+          key={position.id}
+          name={position.name}
+          description={position.description}
+        />
+      )
+    }) ;
+  }
+
   return (
     <div className="mx-5 mt-2">
       <h1>{header}</h1>
       <div className="d-flex justify-content-start flex-wrap w-100">
-        <Card/>
-        <Card/>
-        <Card/>
+        {Cards}
       </div>
     </div>
   );
