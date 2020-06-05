163cf9eea5c65fa8a8eb0fd9b7aa19e82202412f
ilia <ilyakut@gmail.com>
2020-06-05 19:34:43 +0300

    added card component

--- src/components/mainContent/mainContent.js
+++ src/components/mainContent/mainContent.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Header from "../../UI/header/header";
+import Card from "../../UI/card/card";
 
 const MainContent = ({menuItem}) => {
   console.log(menuItem)
+  const header = menuItem?.header;
+  const menuPositions = menuItem?.items;
   return (
-    <div className="main-content">
-      <p>{JSON.stringify(menuItem)}
-      </p>
+    <div className="m-2 px-2 pb-2">
+      <Header text={header}/>
+      <Card/>
     </div>
   );
 };
