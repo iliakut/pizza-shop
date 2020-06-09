e68c2a8c97d7d6c6d0c35a3a303108d165ad8b3b
ilia <ilyakut@gmail.com>
2020-06-09 11:39:14 +0300

    added input.js validation

--- src/UI/input/input.js
+++ src/UI/input/input.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Input = ({inputId, label, placeholder, warningMessage}) => {
+  const [isValid, setValid] = useState(true);
+  const [value, setValue] = useState('');
+
+  const onInput = event => {
+    setValue(event.target.value);
+    checkValid(event.target.value);
+  };
+
+  const checkValid = (value) => {
+    if (value === '') {
+      setValid(false);
+    }
+    else {
+      setValid(true);
+    }
+  };
+
+  let inputClasses = 'form-control';
+  if (!isValid) {
+    inputClasses += ' is-invalid'
+  }
+
   return (
     <div className="form-group">
       <label htmlFor={label}>{label}</label>
-      <input type="text" className="form-control" id={inputId} aria-describedby="emailHelp"
-             placeholder={placeholder}/>
+      <input
+        type="text"
+        className={inputClasses}
+        id={inputId}
+        aria-describedby="emailHelp"
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => onInput(event)}
+        onBlur={() => checkValid(value)}
+      />
       <div className="invalid-feedback">
         {warningMessage}
       </div>
